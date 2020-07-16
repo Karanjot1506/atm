ab7dd1a89e7b921ec430eb634098d77b84fcfc63
karan <karanjot0013@gmail.com>
2020-07-16 23:06:25 +1000

    added a line.

--- atm.py
+++ atm.py
@@ -5,3 +5,4 @@ customers.update({101:{"account_number":101,"balance":2000,"pincode":123,"transa
 customers.update({102:{"account_number":102,"balance":3000,"pincode":123,"transactions":[]}})
 customers.update({103:{"account_number":103,"balance":4000,"pincode":123,"transactions":[]}})
 customers.update({104:{"account_number":104,"balance":5000,"pincode":123,"transactions":[]}})
+print("haha2")
